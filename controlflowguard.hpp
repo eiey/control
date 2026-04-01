c96d822088f731e2b3911234ad977cde2c7c722a
eiey <juanproarmy@gmail.com>
2026-04-02 04:04:49 +0700

    Update controlflowguard.hpp

--- controlflowguard.hpp
+++ controlflowguard.hpp
@@ -1,4 +1,4 @@
-class ControlFlowGuard {
+class controlflowguard {
 private:
     static const uintptr_t BitmapAddress = 0x1ff760;
 
@@ -9,3 +9,17 @@ private:
         PageSize = 0x1000,
         PageMask = 0xFFF
     };
+
+public:
+    static uintptr_t getBitmapAddress() {
+        return BitmapAddress;
+    }
+
+    static uintptr_t calculatePageNumber(uintptr_t address) {
+        return address >> Offsets::PageShift;
+    }
+
+    static uintptr_t calculateByteOffset(uintptr_t address) {
+        return (address >> Offsets::ByteShift) & Offsets::PageMask;
+    }
+};
